70054e314c499c274f29cdccd8b42e6e8fad3c09
Luqaz <igor.shakun.dev@gmail.com>
2016-07-13 03:20:03 +0300

    First try (HOGDescriptor) - unsuccessful

---
+++ Markable/Markable/Markable.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, const char * argv[])
+{
+	Mat img;
+	HOGDescriptor hog;
+	auto detector = HOGDescriptor::getDefaultPeopleDetector();
+	hog.setSVMDetector(detector);
+
+	namedWindow("video capture", CV_WINDOW_AUTOSIZE);
+	while (true)
+	{
+		Mat img = imread("4.jpg", CV_LOAD_IMAGE_COLOR);
+		if (!img.data)
+			continue;
+
+		vector<Rect> found, found_filtered;
+		hog.detectMultiScale(img, found, 0, Size(8, 8), Size(32, 32));
+
+		size_t i, j;
+		for (i = 0; i<found.size(); i++)
+		{
+			Rect r = found[i];
+			for (j = 0; j<found.size(); j++)
+				if (j != i && (r & found[j]) == r)
+					break;
+			if (j == found.size())
+				found_filtered.push_back(r);
+		}
+		for (i = 0; i<found_filtered.size(); i++)
+		{
+			Rect r = found_filtered[i];
+			r.x += cvRound(r.width*0.1);
+			r.width = cvRound(r.width*0.8);
+			r.y += cvRound(r.height*0.06);
+			r.height = cvRound(r.height*0.9);
+			rectangle(img, r.tl(), r.br(), cv::Scalar(0, 255, 0), 2);
+		}
+		imshow("video capture", img);
+		if (waitKey(20) >= 0)
+			break;
+	}
+	return 0;
+}
